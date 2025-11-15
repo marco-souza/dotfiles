b8eea4a2156c9ba73d4b2f1453835b450aa88b7e
Marco <ma.souza.junior@gmail.com>
2025-11-15 12:32:33 -0300

    fea: install mixxx

--- manjaro-post-install.sh
+++ manjaro-post-install.sh
@@ -11,6 +11,7 @@ ensure_installed nvim
 ensure_installed stow
 ensure_installed tmux
 ensure_installed zsh
+ensure_installed mixxx
 ensure_installed yt-dlp
 ensure_installed rg ripgrep
 ensure_installed gh github-cli
